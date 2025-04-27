aa4d85ed2065e55e433c65de62fad5f0553bc3cf
Nic <nicyoong321@gmail.com>
2025-04-27 22:58:34 +0800

    feat: CuboidRegion method

--- src/main/java/com/sunwayMinecraft/benches/CuboidRegion.java
+++ src/main/java/com/sunwayMinecraft/benches/CuboidRegion.java
@@ -1,5 +1,21 @@
 package com.sunwayMinecraft.benches;
 
+import org.bukkit.Location;
+
 public class CuboidRegion {
-    
+    private final String worldName;
+    private final int minX, minY, minZ;
+    private final int maxX, maxY, maxZ;
+
+    public CuboidRegion(String worldName, Location pos1, Location pos2) {
+        this.worldName = worldName;
+
+        this.minX = Math.min(pos1.getBlockX(), pos2.getBlockX());
+        this.minY = Math.min(pos1.getBlockY(), pos2.getBlockY());
+        this.minZ = Math.min(pos1.getBlockZ(), pos2.getBlockZ());
+
+        this.maxX = Math.max(pos1.getBlockX(), pos2.getBlockX());
+        this.maxY = Math.max(pos1.getBlockY(), pos2.getBlockY());
+        this.maxZ = Math.max(pos1.getBlockZ(), pos2.getBlockZ());
+    }
 }
