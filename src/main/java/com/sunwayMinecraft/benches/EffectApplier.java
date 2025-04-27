99cb53628660d789438cef834e4ce7de7f51fc5f
Nic <nicyoong321@gmail.com>
2025-04-27 23:02:09 +0800

    feat: applyRegeneration
    Method to apply Regeneration I on a player that sits on a bench, for a duration of 200 ticks or 10 seconds.

--- src/main/java/com/sunwayMinecraft/benches/EffectApplier.java
+++ src/main/java/com/sunwayMinecraft/benches/EffectApplier.java
@@ -1,7 +1,19 @@
 package com.sunwayMinecraft.benches;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.entity.Player;
 
 public class EffectApplier {
-    
+    private static final int DURATION = 200;
+    private static final int AMPLIFIER = 0;
+
+    public void applyRegeneration(Player player) {
+        player.addPotionEffect(new PotionEffect(
+                PotionEffectType.REGENERATION,
+                200,  // 10 seconds
+                0,    // Level I
+                true, // Ambient particles
+                true  // Show icon
+        ));
+    }
 }
