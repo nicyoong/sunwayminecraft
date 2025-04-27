ec71691b94fdaf708b4518cc361698fe850ba145
Nic <nicyoong321@gmail.com>
2025-04-27 22:46:34 +0800

    feat: new imports
    new imports for BenchesConfigManager.java

--- src/main/java/com/sunwayMinecraft/benches/BenchesConfigManager.java
+++ src/main/java/com/sunwayMinecraft/benches/BenchesConfigManager.java
@@ -1,5 +1,10 @@
 package com.sunwayMinecraft.benches;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+import java.io.File;
+
 public class BenchesConfigManager {
     
 }
