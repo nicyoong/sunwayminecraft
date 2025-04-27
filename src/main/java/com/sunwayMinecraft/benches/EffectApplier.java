37662e4848e0dc60216b3e6a395dbf671a2a2298
Nic <nicyoong321@gmail.com>
2025-04-28 02:03:20 +0800

    chore: remove redundant lines
    Removing redundant lines containing global variables from the EffectApplier class

--- src/main/java/com/sunwayMinecraft/benches/EffectApplier.java
+++ src/main/java/com/sunwayMinecraft/benches/EffectApplier.java
@@ -4,9 +4,6 @@ import org.bukkit.potion.PotionEffectType;
 import org.bukkit.entity.Player;
 
 public class EffectApplier {
-    private static final int DURATION = 200;
-    private static final int AMPLIFIER = 0;
-
     public void applyRegeneration(Player player) {
         player.addPotionEffect(new PotionEffect(
                 PotionEffectType.REGENERATION,
