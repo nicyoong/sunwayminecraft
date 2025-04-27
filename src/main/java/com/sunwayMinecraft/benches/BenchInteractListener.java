ee8521b4d95b3f724e6391f1a8d491c4fa5e96a3
Nic <nicyoong321@gmail.com>
2025-04-27 22:52:57 +0800

    feat: new imports
    Adding the Bukkit imports for the Bench Interact Listener

--- src/main/java/com/sunwayMinecraft/benches/BenchInteractListener.java
+++ src/main/java/com/sunwayMinecraft/benches/BenchInteractListener.java
@@ -1,5 +1,16 @@
 package com.sunwayMinecraft.benches;
 
+import com.sunwayMinecraft.SunwayMinecraft;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.scheduler.BukkitScheduler;
+
 public class BenchInteractListener {
     
 }
