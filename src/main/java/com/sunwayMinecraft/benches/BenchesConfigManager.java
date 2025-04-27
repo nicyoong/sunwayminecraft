deae32191229fb41ad99d4ec6d76495b79ebad28
Nic <nicyoong321@gmail.com>
2025-04-27 22:49:05 +0800

    feat: method
    BeaconConfigManager plugin method to call the initialize config method

--- src/main/java/com/sunwayMinecraft/benches/BenchesConfigManager.java
+++ src/main/java/com/sunwayMinecraft/benches/BenchesConfigManager.java
@@ -6,5 +6,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 
 public class BenchesConfigManager {
+    private final JavaPlugin plugin;
+    private FileConfiguration config;
+
+    public BenchesConfigManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+        // Defer reload to separate method
+        initializeConfig();
+    }
     
 }
