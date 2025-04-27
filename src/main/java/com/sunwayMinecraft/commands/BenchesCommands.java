c4d9ad266ee43b458e55d37626f220dd46e83c4f
Nic <nicyoong321@gmail.com>
2025-04-27 23:06:07 +0800

    feat: BenchesCommands method

--- src/main/java/com/sunwayMinecraft/commands/BenchesCommands.java
+++ src/main/java/com/sunwayMinecraft/commands/BenchesCommands.java
@@ -6,6 +6,12 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class BenchesCommands {
-    
+public class BenchesCommands implements CommandExecutor {
+    private final BenchesConfigManager configManager;
+    private final RegionManager regionManager;
+
+    public BenchesCommands(BenchesConfigManager configManager, RegionManager regionManager) {
+        this.configManager = configManager;
+        this.regionManager = regionManager;
+    }
 }
