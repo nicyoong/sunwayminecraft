1e36a52273413d5debfed9bc7ed7debbe7afd44e
Nic <nicyoong321@gmail.com>
2025-05-03 06:47:48 +0800

    feat: imports
    Imports for SwitchManager.java

--- src/main/java/com/sunwayMinecraft/switches/SwitchManager.java
+++ src/main/java/com/sunwayMinecraft/switches/SwitchManager.java
@@ -1,5 +1,10 @@
 package com.sunwayMinecraft.switches;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+
 public class SwitchManager {
     
 }
