9921f2051cdc32622ef1658c4007e0da7c8195dc
Nic <nicyoong321@gmail.com>
2025-05-03 06:55:17 +0800

    feat: constructor
    Constructor method for SwitchesCommands.

--- src/main/java/com/sunwayMinecraft/commands/SwitchesCommands.java
+++ src/main/java/com/sunwayMinecraft/commands/SwitchesCommands.java
@@ -18,5 +18,20 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class SwitchesCommands {
-    
+    private final JavaPlugin plugin;
+    private final LightConfigManager lightConfig;
+    private final SwitchConfigManager switchConfig;
+    private final LightManager lightManager;
+    private SwitchManager switchManager;
+
+    // Changed to match benches pattern
+    public SwitchesCommands(JavaPlugin plugin,
+                            LightConfigManager lightConfig,
+                            SwitchConfigManager switchConfig) {
+        this.plugin = plugin;
+        this.lightConfig = lightConfig;
+        this.switchConfig = switchConfig;
+        this.lightManager = new LightManager(lightConfig);
+        this.switchManager = new SwitchManager(switchConfig, lightConfig);
+    }
 }
