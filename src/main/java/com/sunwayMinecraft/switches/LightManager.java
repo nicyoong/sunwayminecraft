f102ef942d58011caac1835ce5fac84584dec7c6
Nic <nicyoong321@gmail.com>
2025-05-03 06:45:16 +0800

    feat: imports
    Imports for LightManager()

--- src/main/java/com/sunwayMinecraft/switches/LightManager.java
+++ src/main/java/com/sunwayMinecraft/switches/LightManager.java
@@ -1,5 +1,10 @@
 package com.sunwayMinecraft.switches;
 
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import java.util.*;
+
 public class LightManager {
     
 }
