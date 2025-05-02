d6eb3f549f3b21dedc8495a73e6ab1dfc45f6922
Nic <nicyoong321@gmail.com>
2025-05-03 06:40:18 +0800

    feat: getRegions()
    Method to get the light region

--- src/main/java/com/sunwayMinecraft/switches/LightConfigManager.java
+++ src/main/java/com/sunwayMinecraft/switches/LightConfigManager.java
@@ -24,4 +24,28 @@ public class LightConfigManager {
         }
         lightConfig = YamlConfiguration.loadConfiguration(configFile);
     }
+
+    public Map<String, LightRegion> getRegions() {
+        Map<String, LightRegion> regions = new HashMap<>();
+        ConfigurationSection regionsSection = lightConfig.getConfigurationSection("regions");
+
+        if (regionsSection == null) return regions;
+
+        for (String regionName : regionsSection.getKeys(false)) {
+            String path = "regions." + regionName + ".";
+            World world = plugin.getServer().getWorld(lightConfig.getString(path + "world"));
+
+            regions.put(regionName, new LightRegion(
+                    regionName,
+                    world,
+                    lightConfig.getInt(path + "min.x"),
+                    lightConfig.getInt(path + "min.y"),
+                    lightConfig.getInt(path + "min.z"),
+                    lightConfig.getInt(path + "max.x"),
+                    lightConfig.getInt(path + "max.y"),
+                    lightConfig.getInt(path + "max.z")
+            ));
+        }
+        return regions;
+    }
 }
