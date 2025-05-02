1994c474e675316fada652595601585e62b91d6f
Nic <nicyoong321@gmail.com>
2025-05-03 06:49:13 +0800

    feat: constructor method
    Constructor for the SwitchManager class

--- src/main/java/com/sunwayMinecraft/switches/SwitchManager.java
+++ src/main/java/com/sunwayMinecraft/switches/SwitchManager.java
@@ -6,5 +6,11 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 public class SwitchManager {
-    
+    private final SwitchConfigManager switchConfig;
+    private final LightConfigManager lightConfig;
+
+    public SwitchManager(SwitchConfigManager switchConfig, LightConfigManager lightConfig) {
+        this.switchConfig = switchConfig;
+        this.lightConfig = lightConfig;
+    }
 }
