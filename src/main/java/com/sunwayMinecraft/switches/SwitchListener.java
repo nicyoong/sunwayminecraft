5852a5d5f4b438f634bd4bbef12beffaf1a879e5
Nic <nicyoong321@gmail.com>
2025-05-03 06:51:54 +0800

    feat: Constructor
    Constructor for the SwitchListener class

--- src/main/java/com/sunwayMinecraft/switches/SwitchListener.java
+++ src/main/java/com/sunwayMinecraft/switches/SwitchListener.java
@@ -8,5 +8,11 @@ import java.util.Map;
 import org.bukkit.Location;
 
 public @interface SwitchListener {
-    
+    private final SwitchManager switchManager;
+    private final SwitchConfigManager switchConfig;
+
+    public SwitchListener(SwitchManager switchManager, SwitchConfigManager switchConfig) {
+        this.switchManager = switchManager;
+        this.switchConfig = switchConfig;
+    }
 }
