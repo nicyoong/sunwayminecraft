00d84661e25c3b21faf8ffcd635122e202bd9ee8
Nic <nicyoong321@gmail.com>
2025-05-03 06:40:59 +0800

    feat: record class LightRegion

--- src/main/java/com/sunwayMinecraft/switches/LightRegion.java
+++ src/main/java/com/sunwayMinecraft/switches/LightRegion.java
@@ -1,5 +1,18 @@
 package com.sunwayMinecraft.switches;
 
-public class LightRegion {
-    
+import org.bukkit.Location;
+import org.bukkit.World;
+
+public record LightRegion(
+        String name,
+        World world,
+        int minX, int minY, int minZ,
+        int maxX, int maxY, int maxZ
+) {
+    public boolean contains(Location loc) {
+        return loc.getWorld().equals(world) &&
+                loc.getBlockX() >= minX && loc.getBlockX() <= maxX &&
+                loc.getBlockY() >= minY && loc.getBlockY() <= maxY &&
+                loc.getBlockZ() >= minZ && loc.getBlockZ() <= maxZ;
+    }
 }
