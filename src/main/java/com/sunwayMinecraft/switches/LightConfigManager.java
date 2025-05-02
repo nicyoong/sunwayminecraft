724b9ee7dd59013c90efacfdcc951921231e171c
Nic <nicyoong321@gmail.com>
2025-05-03 06:39:09 +0800

    feat: imports
    Imports for LightConfigManager

--- src/main/java/com/sunwayMinecraft/switches/LightConfigManager.java
+++ src/main/java/com/sunwayMinecraft/switches/LightConfigManager.java
@@ -1,5 +1,13 @@
 package com.sunwayMinecraft.switches;
 
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.World;
+import java.io.File;
+import java.util.*;
+
 public class LightConfigManager {
     
 }
