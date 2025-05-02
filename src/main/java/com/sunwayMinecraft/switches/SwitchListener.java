507fdcbd331687eff5a0474847d288057aef4376
Nic <nicyoong321@gmail.com>
2025-05-03 06:51:37 +0800

    feat: imports
    imports for the SwitchListener class

--- src/main/java/com/sunwayMinecraft/switches/SwitchListener.java
+++ src/main/java/com/sunwayMinecraft/switches/SwitchListener.java
@@ -1,5 +1,12 @@
 package com.sunwayMinecraft.switches;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.block.Block;
+import java.util.Map;
+import org.bukkit.Location;
+
 public @interface SwitchListener {
     
 }
