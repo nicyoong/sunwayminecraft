7180ff54060b93cf11b6956397367ba8c7c1e4d0
Nic <nicyoong321@gmail.com>
2025-05-03 06:42:52 +0800

    feat: imports
    Imports for SwitchConfigManager.java

--- src/main/java/com/sunwayMinecraft/switches/SwitchConfigManager.java
+++ src/main/java/com/sunwayMinecraft/switches/SwitchConfigManager.java
@@ -1,5 +1,14 @@
 package com.sunwayMinecraft.switches;
 
+import org.bukkit.World;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Location;
+import java.io.File;
+import java.util.*;
+
 public class SwitchConfigManager {
     
 }
