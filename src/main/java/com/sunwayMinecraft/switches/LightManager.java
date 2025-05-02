9bc5d63db12e1e8e8daa3a5651fc3dd57a680015
Nic <nicyoong321@gmail.com>
2025-05-03 06:45:30 +0800

    feat: LightManager method

--- src/main/java/com/sunwayMinecraft/switches/LightManager.java
+++ src/main/java/com/sunwayMinecraft/switches/LightManager.java
@@ -6,5 +6,15 @@ import org.bukkit.entity.Player;
 import java.util.*;
 
 public class LightManager {
-    
+    private static final Map<Material, Material> LIGHT_MAPPINGS = Map.of(
+            Material.SEA_LANTERN, Material.WHITE_CONCRETE,
+            Material.GLOWSTONE, Material.COBBLESTONE,
+            Material.JACK_O_LANTERN, Material.CARVED_PUMPKIN
+    );
+
+    private final LightConfigManager configManager;
+
+    public LightManager(LightConfigManager configManager) {
+        this.configManager = configManager;
+    }
 }
