294aa37dd0662b65f32e76076b8ae9e2a6343409
Nic <nicyoong321@gmail.com>
2025-05-10 08:22:36 +0800

    feat: imports

--- src/main/java/com/sunwayMinecraft/cathealer/HealingSystem.java
+++ src/main/java/com/sunwayMinecraft/cathealer/HealingSystem.java
@@ -1,3 +1,14 @@
+package com.sunwayMinecraft.cathealer;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Cat;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.attribute.Attribute;
+import java.util.Collection;
+
 public class HealingSystem {
     
 }
