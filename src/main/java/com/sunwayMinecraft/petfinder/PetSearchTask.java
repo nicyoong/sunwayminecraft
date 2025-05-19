e7857e55de787b275834a28ba667d2c10d95bd9e
nicyoong <nicyoong321@gmail.com>
2025-05-20 04:43:31 +0800

    constructor
    petsearchtask

--- src/main/java/com/sunwayMinecraft/petfinder/PetSearchTask.java
+++ src/main/java/com/sunwayMinecraft/petfinder/PetSearchTask.java
@@ -8,5 +8,19 @@ import org.bukkit.util.BoundingBox;
 import java.util.*;
 
 public class PetSearchTask extends BukkitRunnable {
-    
+    private final CommandSender sender;
+    private final List<Entity> entities;
+    private final UUID targetUUID;
+    private final BoundingBox area;
+    private final PetFinderManager manager;
+    private final List<String> results = new ArrayList<>();
+
+    public PetSearchTask(Main plugin, CommandSender sender, List<Entity> entities,
+                         UUID targetUUID, BoundingBox area, PetFinderManager manager) {
+        this.sender = sender;
+        this.entities = new ArrayList<>(entities);
+        this.targetUUID = targetUUID;
+        this.area = area;
+        this.manager = manager;
+    }
 }
