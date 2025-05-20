90d62b491742d56c9702b628bc7d89fa072403fb
nicyoong <nicyoong321@gmail.com>
2025-05-20 18:44:22 +0800

    Constructor

--- src/main/java/com/sunwayMinecraft/switches/MidnightLightScheduler.java
+++ src/main/java/com/sunwayMinecraft/switches/MidnightLightScheduler.java
@@ -5,6 +5,11 @@ import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.scheduler.BukkitRunnable;
 
-public class MidnightLightScheduler {
+public class MidnightLightScheduler extends BukkitRunnable {
+    private final SwitchConfigManager switchConfig;
+    private long lastTriggeredTime = -1;
 
+    public MidnightLightScheduler(SwitchConfigManager switchConfig) {
+        this.switchConfig = switchConfig;
+    }
 }
