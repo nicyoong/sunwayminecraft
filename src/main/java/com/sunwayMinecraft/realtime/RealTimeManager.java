b765888523b8cbbf7960e4b89af9957fbb303c20
nicyoong <nicyoong321@gmail.com>
2025-05-21 20:05:46 +0800

    remove unnecessary imports

--- src/main/java/com/sunwayMinecraft/realtime/RealTimeManager.java
+++ src/main/java/com/sunwayMinecraft/realtime/RealTimeManager.java
@@ -1,12 +1,9 @@
 package com.sunwayMinecraft.realtime;
 
-import com.sunwayMinecraft.utils.ConfigLoader;
-import org.bukkit.plugin.java.JavaPlugin;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class RealTimeManager {
-    private final JavaPlugin plugin;
     private DateTimeFormatter timeFormatter;
     private DateTimeFormatter dateFormatter;
 
