a696b39f09701d455f0eae35e582f7d7b85962ea
nicyoong <nicyoong321@gmail.com>
2025-05-21 19:02:35 +0800

    constructor

--- src/main/java/com/sunwayMinecraft/realtime/RealTimeManager.java
+++ src/main/java/com/sunwayMinecraft/realtime/RealTimeManager.java
@@ -6,5 +6,11 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class RealTimeManager {
+    private final JavaPlugin plugin;
+    private DateTimeFormatter timeFormatter;
+    private DateTimeFormatter dateFormatter;
 
+    public TimeManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
 }
