78c9813e5462c964b8bc6380d9e27cb39e501f71
Nic <73386479+nicyoong@users.noreply.github.com>
2025-05-23 06:41:39 +0800

    style: reformat code according to Google Java Style Guide

--- src/main/java/com/sunwayMinecraft/switches/SwitchManager.java
+++ src/main/java/com/sunwayMinecraft/switches/SwitchManager.java
@@ -10,14 +10,16 @@ import org.bukkit.entity.Player;
  * configurations to handle toggling of light blocks when a switch is activated.
  *
  * <p>This manager is responsible for:
+ *
  * <ul>
- *   <li>Retrieving the configured light locations for a given {@link ButtonSwitch}.</li>
- *   <li>Determining the current material at each location and computing its toggle target
- *       (light ↔ non-light) using {@link LightManager}.</li>
- *   <li>Applying the material change to each block in the switch’s lightLocations.</li>
+ *   <li>Retrieving the configured light locations for a given {@link ButtonSwitch}.
+ *   <li>Determining the current material at each location and computing its toggle target (light ↔
+ *       non-light) using {@link LightManager}.
+ *   <li>Applying the material change to each block in the switch’s lightLocations.
  * </ul>
  *
  * <p>Usage example:
+ *
  * <pre>{@code
  * SwitchManager switchMgr = new SwitchManager(switchConfigManager, lightConfigManager);
  * ButtonSwitch button = switchConfigManager.getSwitches().get(clickedLocation);
@@ -25,25 +27,29 @@ import org.bukkit.entity.Player;
  * }</pre>
  */
 public class SwitchManager {
-    private final SwitchConfigManager switchConfig;
-    private final LightConfigManager lightConfig;
+  private final SwitchConfigManager switchConfig;
+  private final LightConfigManager lightConfig;
 
-    public SwitchManager(SwitchConfigManager switchConfig, LightConfigManager lightConfig) {
-        this.switchConfig = switchConfig;
-        this.lightConfig = lightConfig;
-    }
+  public SwitchManager(SwitchConfigManager switchConfig, LightConfigManager lightConfig) {
+    this.switchConfig = switchConfig;
+    this.lightConfig = lightConfig;
+  }
 
-    public void toggleLights(ButtonSwitch buttonSwitch, Player player) {
-        buttonSwitch.lightLocations().forEach(loc -> {
-            Block block = loc.getBlock();
-            Material current = block.getType();
-            Material target = LightManager.isLightBlock(current) ?
-                    LightManager.getOffMaterial(current) :
-                    LightManager.getOriginalMaterial(current);
+  public void toggleLights(ButtonSwitch buttonSwitch, Player player) {
+    buttonSwitch
+        .lightLocations()
+        .forEach(
+            loc -> {
+              Block block = loc.getBlock();
+              Material current = block.getType();
+              Material target =
+                  LightManager.isLightBlock(current)
+                      ? LightManager.getOffMaterial(current)
+                      : LightManager.getOriginalMaterial(current);
 
-            if (target != null) {
+              if (target != null) {
                 block.setType(target);
-            }
-        });
-    }
+              }
+            });
+  }
 }
