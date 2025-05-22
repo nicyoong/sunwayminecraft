17999fb9dbdc0b148a44ceef1a6fc5d63304e70b
Nic <73386479+nicyoong@users.noreply.github.com>
2025-05-23 06:33:28 +0800

    style: reformat code according to Google Java Style Guide

--- src/main/java/com/sunwayMinecraft/switches/ButtonSwitch.java
+++ src/main/java/com/sunwayMinecraft/switches/ButtonSwitch.java
@@ -3,7 +3,4 @@ package com.sunwayMinecraft.switches;
 import org.bukkit.Location;
 import java.util.List;
 
-public record ButtonSwitch(
-        Location buttonLocation,
-        List<Location> lightLocations
-) {}
+public record ButtonSwitch(Location buttonLocation, List<Location> lightLocations) {}
