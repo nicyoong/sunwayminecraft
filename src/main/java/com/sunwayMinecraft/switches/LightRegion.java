55a8d0841abdba21647ae2875b9299ac3ea26eae
Nic <73386479+nicyoong@users.noreply.github.com>
2025-05-23 06:33:58 +0800

    style: reformat code according to Google Java Style Guide

--- src/main/java/com/sunwayMinecraft/switches/LightRegion.java
+++ src/main/java/com/sunwayMinecraft/switches/LightRegion.java
@@ -4,15 +4,14 @@ import org.bukkit.Location;
 import org.bukkit.World;
 
 public record LightRegion(
-        String name,
-        World world,
-        int minX, int minY, int minZ,
-        int maxX, int maxY, int maxZ
-) {
-    public boolean contains(Location loc) {
-        return loc.getWorld().equals(world) &&
-                loc.getBlockX() >= minX && loc.getBlockX() <= maxX &&
-                loc.getBlockY() >= minY && loc.getBlockY() <= maxY &&
-                loc.getBlockZ() >= minZ && loc.getBlockZ() <= maxZ;
-    }
+    String name, World world, int minX, int minY, int minZ, int maxX, int maxY, int maxZ) {
+  public boolean contains(Location loc) {
+    return loc.getWorld().equals(world)
+        && loc.getBlockX() >= minX
+        && loc.getBlockX() <= maxX
+        && loc.getBlockY() >= minY
+        && loc.getBlockY() <= maxY
+        && loc.getBlockZ() >= minZ
+        && loc.getBlockZ() <= maxZ;
+  }
 }
