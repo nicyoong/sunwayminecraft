071844648fcf6ae2c9833f1a5d36ac30614c732f
Nic <73386479+nicyoong@users.noreply.github.com>
2025-05-23 04:57:31 +0800

    style: reformat code according to Google Java Style Guide

--- src/main/java/com/sunwayMinecraft/benches/EffectApplier.java
+++ src/main/java/com/sunwayMinecraft/benches/EffectApplier.java
@@ -1,17 +1,19 @@
 package com.sunwayMinecraft.benches;
+
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import org.bukkit.entity.Player;
 
 public class EffectApplier {
-    public void applyRegeneration(Player player) {
-        player.addPotionEffect(new PotionEffect(
-                PotionEffectType.REGENERATION,
-                200,  // 10 seconds
-                0,    // Level I
-                true, // Ambient particles
-                true  // Show icon
-        ));
-    }
+  public void applyRegeneration(Player player) {
+    player.addPotionEffect(
+        new PotionEffect(
+            PotionEffectType.REGENERATION,
+            200, // 10 seconds
+            0, // Level I
+            true, // Ambient particles
+            true // Show icon
+            ));
+  }
 }
