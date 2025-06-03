dc0bc5e157585dcb69c9ce8302d4f41d87a3a5c4
Nic <73386479+nicyoong@users.noreply.github.com>
2025-06-04 04:18:49 +0800

    Create CoinFlipCommands.java

---
+++ src/main/java/com/sunwayMinecraft/commands/CoinFlipCommands.java
@@ -0,0 +1,12 @@
+package com.sunwayMinecraft.commands;
+
+import com.sunwayMinecraft.coinflip.CoinFlipSystem;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
