b09ac75f8c80350479934f5afd1b63719426111f
Nic <73386479+nicyoong@users.noreply.github.com>
2025-06-04 04:19:50 +0800

    oncommand

--- src/main/java/com/sunwayMinecraft/commands/CoinFlipCommands.java
+++ src/main/java/com/sunwayMinecraft/commands/CoinFlipCommands.java
@@ -17,3 +17,17 @@ public class CoinFlipCommands implements CommandExecutor, TabCompleter {
     public CoinFlipCommands(CoinFlipSystem coinFlipSystem) {
         this.coinFlipSystem = coinFlipSystem;
     }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "Only players can use this command!");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        if (args.length == 0) {
+            sendHelp(player);
+            return true;
+        }
