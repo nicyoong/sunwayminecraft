c12b5d2da5562a3566652ac8b37bf0cf198e134a
Nic <73386479+nicyoong@users.noreply.github.com>
2025-06-04 06:01:59 +0800

    Create ItemCoinFlipSystem.java

---
+++ src/main/java/com/sunwayMinecraft/coinflip/ItemCoinFlipSystem.java
@@ -0,0 +1,8 @@
+package com.sunwayMinecraft.coinflip;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+import java.util.HashMap;
+import java.util.Map;
