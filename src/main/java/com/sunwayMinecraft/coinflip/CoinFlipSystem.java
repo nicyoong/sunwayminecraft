6578b724d731b891e36d45330941faff33a2f6c0
Nic <73386479+nicyoong@users.noreply.github.com>
2025-06-04 04:14:57 +0800

    Create CoinFlipSystem.java

---
+++ src/main/java/com/sunwayMinecraft/coinflip/CoinFlipSystem.java
@@ -0,0 +1,10 @@
+package com.sunwayMinecraft.coinflip;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.UUID;
