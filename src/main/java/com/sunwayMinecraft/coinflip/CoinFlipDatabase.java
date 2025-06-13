15f70dee9754f57c4ac0a3f1f4f849758ccbe26c
Nic <73386479+nicyoong@users.noreply.github.com>
2025-06-13 19:58:51 +0800

    Constructor

--- src/main/java/com/sunwayMinecraft/coinflip/CoinFlipDatabase.java
+++ src/main/java/com/sunwayMinecraft/coinflip/CoinFlipDatabase.java
@@ -6,5 +6,11 @@ import java.sql.*;
 import java.util.UUID;
 
 public class CoinFlipDatabase {
-    
+  private final JavaPlugin plugin;
+  private Connection connection;
+
+  public CoinFlipDatabase(JavaPlugin plugin) {
+    this.plugin = plugin;
+    initializeDatabase();
+  }
 }
