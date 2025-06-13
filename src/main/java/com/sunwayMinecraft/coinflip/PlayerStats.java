3114ce74988e23367abdd7c96f7604f66b9bd524
Nic <73386479+nicyoong@users.noreply.github.com>
2025-06-13 19:50:46 +0800

    variables and constructor

--- src/main/java/com/sunwayMinecraft/coinflip/PlayerStats.java
+++ src/main/java/com/sunwayMinecraft/coinflip/PlayerStats.java
@@ -3,5 +3,17 @@ package com.sunwayMinecraft.coinflip;
 import java.util.UUID;
 
 public class PlayerStats {
-    
+    private final UUID uuid;
+    private int moneyWins;
+    private int moneyLosses;
+    private double moneyWagered;
+    private double moneyWon;
+    private int itemWins;
+    private int itemLosses;
+    private int itemsWagered;
+    private int itemsWon;
+
+    public PlayerStats(UUID uuid) {
+        this.uuid = uuid;
+    }
 }
