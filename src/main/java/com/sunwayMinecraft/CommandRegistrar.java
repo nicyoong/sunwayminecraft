5419f445e82ccd3a8a385166e2673d823ea0d4f6
Nic <73386479+nicyoong@users.noreply.github.com>
2025-06-14 21:38:44 +0800

    Create CommandRegistrar.java

---
+++ src/main/java/com/sunwayMinecraft/CommandRegistrar.java
@@ -0,0 +1,16 @@
+package com.sunwayMinecraft;
+
+import com.sunwayMinecraft.commands.*;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CommandRegistrar {
+
+  private final JavaPlugin plugin;
+
+  public CommandRegistrar(JavaPlugin plugin) {
+    this.plugin = plugin;
+  }
+
+  
+}
