afb9737c94be57df5765caa93c019adb5d368611
Nic <73386479+nicyoong@users.noreply.github.com>
2026-04-18 03:50:35 +0800

    Constructor for class billing service

--- src/main/java/com/sunwayMinecraft/residency/leasing/BillingService.java
+++ src/main/java/com/sunwayMinecraft/residency/leasing/BillingService.java
@@ -7,3 +7,13 @@ import org.bukkit.OfflinePlayer;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+
+public class BillingService {
+    private final ResidencyManager manager;
+    private final Economy economy;
+
+    public BillingService(ResidencyManager manager, Economy economy) {
+        this.manager = manager;
+        this.economy = economy;
+    }
+
