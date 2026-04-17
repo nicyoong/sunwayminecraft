7eb237ccc2d621ccd0986a9d54ade45fc4d18778
Nic <73386479+nicyoong@users.noreply.github.com>
2026-04-18 03:50:19 +0800

    Create BillingService.java

---
+++ src/main/java/com/sunwayMinecraft/residency/leasing/BillingService.java
@@ -0,0 +1,9 @@
+package com.sunwayMinecraft.residency.leasing;
+
+import com.sunwayMinecraft.residency.ResidencyManager;
+import com.sunwayMinecraft.residency.domain.*;
+        import net.milkbowl.vault.economy.Economy;
+import org.bukkit.OfflinePlayer;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
