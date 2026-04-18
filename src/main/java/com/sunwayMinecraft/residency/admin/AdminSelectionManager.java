41988ce223e4502364e1666bb3eeda62d85bacaf
Nic <73386479+nicyoong@users.noreply.github.com>
2026-04-18 23:10:20 +0800

    Create AdminSelectionManager.java

---
+++ src/main/java/com/sunwayMinecraft/residency/admin/AdminSelectionManager.java
@@ -0,0 +1,9 @@
+package com.sunwayMinecraft.residency.admin;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.*;
+
