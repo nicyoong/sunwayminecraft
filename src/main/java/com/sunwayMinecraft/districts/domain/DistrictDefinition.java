7b228b435d7a33bf9f2a86fe397e9e834b2e2f24
Nic <73386479+nicyoong@users.noreply.github.com>
2026-04-23 11:48:11 +0800

    Create DistrictDefinition.java

---
+++ src/main/java/com/sunwayMinecraft/districts/domain/DistrictDefinition.java
@@ -0,0 +1,8 @@
+package com.sunwayMinecraft.districts.domain;
+
+import com.sunwayMinecraft.districts.region.Region3i;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
