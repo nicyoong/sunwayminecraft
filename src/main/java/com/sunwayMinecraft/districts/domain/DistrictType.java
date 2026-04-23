0ecfd23c71a8e407c9824ee33d47ecf562a56dc3
Nic <73386479+nicyoong@users.noreply.github.com>
2026-04-23 11:48:41 +0800

    Create DistrictType.java

---
+++ src/main/java/com/sunwayMinecraft/districts/domain/DistrictType.java
@@ -0,0 +1,11 @@
+package com.sunwayMinecraft.districts.domain;
+
+public enum DistrictType {
+    RESIDENTIAL,
+    COMMERCIAL,
+    OFFICE,
+    MARKET,
+    ENTERTAINMENT,
+    CIVIC,
+    MIXED_USE
+}
