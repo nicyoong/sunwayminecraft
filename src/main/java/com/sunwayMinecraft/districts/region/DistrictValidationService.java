82a3d6de3c1ccddf679cb5b0f2109ec6c8089982
Nic <73386479+nicyoong@users.noreply.github.com>
2026-04-23 11:50:46 +0800

    constructor

--- src/main/java/com/sunwayMinecraft/districts/region/DistrictValidationService.java
+++ src/main/java/com/sunwayMinecraft/districts/region/DistrictValidationService.java
@@ -8,3 +8,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.ArrayList;
 import java.util.List;
 
+public class DistrictValidationService {
+    private static final long MAX_REGION_VOLUME = 1_000_000L;
+
+    private final JavaPlugin plugin;
+    private final DistrictsConfigManager configManager;
+
+    public DistrictValidationService(JavaPlugin plugin, DistrictsConfigManager configManager) {
+        this.plugin = plugin;
+        this.configManager = configManager;
+    }
+
