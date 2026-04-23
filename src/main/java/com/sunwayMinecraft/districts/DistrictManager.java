cbf823caa06ae1f67131dc1f3aa889f0a7b58216
Nic <73386479+nicyoong@users.noreply.github.com>
2026-04-23 11:51:17 +0800

    Create DistrictManager.java

---
+++ src/main/java/com/sunwayMinecraft/districts/DistrictManager.java
@@ -0,0 +1,11 @@
+package com.sunwayMinecraft.districts;
+
+import com.sunwayMinecraft.districts.config.DistrictsConfigManager;
+import com.sunwayMinecraft.districts.domain.DistrictDefinition;
+import com.sunwayMinecraft.districts.region.DistrictResolver;
+import com.sunwayMinecraft.districts.region.DistrictValidationService;
+import org.bukkit.Location;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+
