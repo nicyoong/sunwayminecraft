7580dca92a1449e833762a87d97560f51cb27bea
Nic <73386479+nicyoong@users.noreply.github.com>
2026-04-23 11:51:27 +0800

    constructor

--- src/main/java/com/sunwayMinecraft/districts/DistrictManager.java
+++ src/main/java/com/sunwayMinecraft/districts/DistrictManager.java
@@ -9,3 +9,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
 
+public class DistrictManager {
+    private final JavaPlugin plugin;
+    private final DistrictsConfigManager configManager;
+    private final DistrictResolver resolver;
+    private final DistrictValidationService validationService;
+
+    public DistrictManager(JavaPlugin plugin, DistrictsConfigManager configManager) {
+        this.plugin = plugin;
+        this.configManager = configManager;
+        this.resolver = new DistrictResolver(configManager);
+        this.validationService = new DistrictValidationService(plugin, configManager);
+    }
+
