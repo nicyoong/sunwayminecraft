3e36c33651c5771623e5129f47c69f51936bdb5f
Nic <73386479+nicyoong@users.noreply.github.com>
2026-04-23 11:50:32 +0800

    Create DistrictValidationService.java

---
+++ src/main/java/com/sunwayMinecraft/districts/region/DistrictValidationService.java
@@ -0,0 +1,10 @@
+package com.sunwayMinecraft.districts.region;
+
+import com.sunwayMinecraft.districts.config.DistrictsConfigManager;
+import com.sunwayMinecraft.districts.domain.DistrictDefinition;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
