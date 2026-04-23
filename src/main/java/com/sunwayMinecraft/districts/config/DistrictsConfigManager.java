5c0648e1bbabcc92fb58fa0049939e63e43aae07
Nic <73386479+nicyoong@users.noreply.github.com>
2026-04-23 11:49:07 +0800

    Create DistrictsConfigManager.java

---
+++ src/main/java/com/sunwayMinecraft/districts/config/DistrictsConfigManager.java
@@ -0,0 +1,13 @@
+package com.sunwayMinecraft.districts.config;
+
+import com.sunwayMinecraft.districts.domain.ApprovalBias;
+import com.sunwayMinecraft.districts.domain.DistrictDefinition;
+import com.sunwayMinecraft.districts.domain.DistrictType;
+import com.sunwayMinecraft.districts.region.Region3i;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.*;
+
