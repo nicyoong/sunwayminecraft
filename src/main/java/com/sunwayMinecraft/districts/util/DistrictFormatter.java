66ea03424e39b0ac1e8d6f07f4a6044e5be58701
Nic <73386479+nicyoong@users.noreply.github.com>
2026-04-23 11:51:04 +0800

    Create DistrictFormatter.java

---
+++ src/main/java/com/sunwayMinecraft/districts/util/DistrictFormatter.java
@@ -0,0 +1,7 @@
+package com.sunwayMinecraft.districts.util;
+
+import com.sunwayMinecraft.districts.domain.DistrictDefinition;
+import com.sunwayMinecraft.districts.domain.DistrictType;
+
+import java.util.stream.Collectors;
+
